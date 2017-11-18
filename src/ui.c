4961f48ffc95589bbf5dea17641ae65119870ff5
Tom Eccles <t@freedommail.info>
2017-11-18 12:45:10 +0000

    Beginnings of menu bar with stub handlers

--- src/ui.c
+++ src/ui.c
@@ -21,6 +21,7 @@ static void shutdown_handler(__attribute__((unused)) GApplication *app, gpointer
 
 static EdsacErrorNotebook *notebook = NULL;
 static GtkStatusbar *bar = NULL;
+static GtkWindow *main_window = NULL;
 
 // functions
 
@@ -67,20 +68,90 @@ void gui_update(gpointer g_idle_id) {
     update_bar();
 }
 
+// handles the quit action
+static void quit_activate(void) {
+    if (NULL != main_window) {
+        gtk_window_close(main_window);
+    }
+}
+
+// handles the add_node action
+static void add_node_activate(void) {
+    puts("Add node");
+}
+
+static void show_disabled_change_state(GSimpleAction *simple) {
+    assert(NULL != simple);
+    gboolean show_disabled = g_variant_get_boolean(g_action_get_state(G_ACTION(simple)));
+
+    puts("In handler");
+    // toggle
+    if (show_disabled) {
+        g_simple_action_set_state(simple, g_variant_new_boolean(FALSE));
+        puts("Now Not showing disabled items");
+    } else {
+        g_simple_action_set_state(simple, g_variant_new_boolean(TRUE));
+        puts("Now Showing disabled items");
+    }
+}
+
+typedef void (*action_handler_t)(GSimpleAction *simple, GVariant *parameter, gpointer user_data);
+
 // activate handler for the application
 static void activate(GtkApplication *app, __attribute__((unused)) gpointer data) {
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "EDSAC Status Monitor");
+    main_window = GTK_WINDOW(gtk_application_window_new(app));
+    gtk_window_set_title(main_window, "EDSAC Status Monitor");
     //gtk_window_maximize(GTK_WINDOW(WINDOW));
 
     // set minimum window size
     GdkGeometry geometry;
     geometry.min_height = 400;
     geometry.min_width = 600;
-    gtk_window_set_geometry_hints(GTK_WINDOW(window), NULL, &geometry, GDK_HINT_MIN_SIZE);
+    gtk_window_set_geometry_hints(main_window, NULL, &geometry, GDK_HINT_MIN_SIZE);
 
     // GTKBox to hold stuff
     GtkBox *box = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 0 /*arbitrary: a guess*/));
+
+    // Menu bar actions
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+    static const GActionEntry actions[] = {
+        {"add_node", (action_handler_t) add_node_activate},
+        {"quit", (action_handler_t) quit_activate},
+        {"show_disabled", NULL, "b", "true", (action_handler_t) show_disabled_change_state}
+    };
+    #pragma GCC diagnostic pop
+    g_action_map_add_action_entries(G_ACTION_MAP(app), actions, G_N_ELEMENTS(actions), NULL);
+
+    // File menu model 
+    GMenu *file = g_menu_new();
+    assert(NULL != file);
+    g_menu_append(file, "Add Node", "app.add_node");
+    g_menu_append(file, "Quit", "app.quit");
+    const char *quit_accels[] = {"<Control>Q", NULL};
+    gtk_application_set_accels_for_action(app, "app.quit", quit_accels);
+    g_menu_freeze(file);
+
+    // View menu model
+    GMenu *view = g_menu_new();
+    assert(NULL != view);
+    GMenuItem *show_disabled = g_menu_item_new("Show Disabled", "app.show_disabled");
+    assert(NULL != show_disabled);
+    g_menu_item_set_action_and_target_value(show_disabled, "app.show_disabled", g_variant_new_boolean(TRUE));
+    g_menu_append_item(view, show_disabled);
+    //g_menu_freeze(view);
+
+    // Menu bar model
+    GMenu *model = g_menu_new();
+    assert(NULL != model);
+    g_menu_append_submenu(model, "File", G_MENU_MODEL(file));
+    g_menu_append_submenu(model, "View", G_MENU_MODEL(view));
+    //g_menu_freeze(model);
+
+    // Menu bar widget
+    GtkWidget *menu = gtk_menu_bar_new_from_model(G_MENU_MODEL(model));
+    assert(NULL != menu);
+    gtk_box_pack_start(box, menu, FALSE, FALSE, 0);
 
     // make notebook
     notebook = edsac_error_notebook_new();
@@ -93,8 +164,8 @@ static void activate(GtkApplication *app, __attribute__((unused)) gpointer data)
     update_bar();
     gtk_box_pack_start(box, GTK_WIDGET(bar), FALSE, FALSE, 0);
 
-    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(box));
-    gtk_widget_show_all(window);
+    gtk_container_add(GTK_CONTAINER(main_window), GTK_WIDGET(box));
+    gtk_widget_show_all(GTK_WIDGET(main_window));
 }
 
 // handler called just before we terminate
